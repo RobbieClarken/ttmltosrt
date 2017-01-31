14a010dc652053138b17862e11a0c1d4d9791acc
RobbieClarken <robbie.clarken@gmail.com>
2017-01-31 15:37:52 +1100

    Bump version to 1.2.0

--- ttmltosrt/__init__.py
+++ ttmltosrt/__init__.py
@@ -1,5 +1,5 @@
 from .ttmltosrt import convert_file, srt_generator
 
 
-__version__ = '1.1.0'
-__all__ = [convert_file, srt_generator]
+__version__ = '1.2.0'
+__all__ = ['convert_file', 'srt_generator']
